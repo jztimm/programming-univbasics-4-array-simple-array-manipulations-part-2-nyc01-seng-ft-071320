7769f24b58315666c3f5a001de75856c781c6b55
Jzavier Timm <jztimm@gmail.com>
2020-06-30 16:22:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,9 +3,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  if array[4] == true
-    array.insert(element)
-  end
+  array.insert(element)
 end
 
 def using_uniq(array)
